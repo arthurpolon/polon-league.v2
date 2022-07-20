fc9eb5422594894a6977ca852420ca4b0092bcbb
ARTHUR POLON LEMOS <arthur.lemos@afya.com.br>
2022-07-20 16:48:09 -0300

    📦 NEW: add theme change to summoner page, change background waves

--- pages/summoner/index.tsx
+++ pages/summoner/index.tsx
@@ -10,30 +10,36 @@ const SummonerPage = () => {
   const { isDark, toggleTheme } = useTheme();
 
   return (
-    <main className="w-11/12 max-w-7xl mx-auto mt-10">
-      <div className="flex justify-between items-center">
-        <Link href="/">
-          <div className="cursor-pointer flex w-fit">
-            <HomeIcon className="mr-2" />
-            <span className="font-medium text-lg">Back To Home</span>
-          </div>
-        </Link>
+    <main className="w-screen h-screen bg-pink-50 relative z-0 dark:bg-slate-800">
+      <div className="w-11/12 max-w-7xl mx-auto pt-10">
+        <div className="flex justify-between items-center dark:text-gray-100">
+          <Link href="/">
+            <div className="cursor-pointer flex w-fit">
+              <HomeIcon className="mr-2" />
+              <span className="font-medium text-lg">Back To Home</span>
+            </div>
+          </Link>
 
-        <button
-          type="button"
-          onClick={toggleTheme}
-          className="border-2 p-2 border-black rounded-md dark:border-gray-200 relative w-10 h-10"
-        >
-          {isDark ? (
-            <SunIcon className="absolute text-gray-300 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 " />
-          ) : (
-            <MoonIcon className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 " />
-          )}
-        </button>
+          <button
+            type="button"
+            onClick={toggleTheme}
+            className="border-2 p-2 border-black rounded-md dark:border-gray-100 relative w-10 h-10"
+          >
+            {isDark ? (
+              <SunIcon className="absolute text-gray-100 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 " />
+            ) : (
+              <MoonIcon className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 " />
+            )}
+          </button>
+        </div>
       </div>
 
-      <div className="fixed inset-0 -z-50 pointer-events-none">
-        <Image src="/waves-background.jpg" layout="fill" objectFit="cover" />
+      <div className="fixed inset-0 -z-10 pointer-events-none">
+        <Image
+          src={isDark ? '/bg-waves-dark.svg' : '/bg-waves-light.svg'}
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
     </main>
   );
