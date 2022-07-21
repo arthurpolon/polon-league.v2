15c7da7623da62c8f2d8fa6066e028429dccae05
ARTHUR POLON LEMOS <arthur.lemos@afya.com.br>
2022-07-21 11:15:35 -0300

    📦 NEW: create form in summoner page

--- pages/summoner/index.tsx
+++ pages/summoner/index.tsx
@@ -1,13 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useTheme } from '../../contexts/theme';
+import ThemeSwitch from 'components/ThemeSwitch';
+import { useTheme } from 'contexts/theme';
 
-import HomeIcon from '../../public/home-icon.svg';
-import MoonIcon from '../../public/moon-icon.svg';
-import SunIcon from '../../public/sun-icon.svg';
+import Logo from 'public/logo.svg';
+
+import HomeIcon from 'public/home-icon.svg';
+import { FormEvent } from 'react';
 
 const SummonerPage = () => {
-  const { isDark, toggleTheme } = useTheme();
+  const { isDark } = useTheme();
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const summonerName = formData.get('summoner-name') as string;
+
+    console.log(summonerName);
+  };
 
   return (
     <main className="w-screen h-screen bg-pink-50 relative z-0 dark:bg-slate-800">
@@ -20,18 +31,39 @@ const SummonerPage = () => {
             </div>
           </Link>
 
-          <button
-            type="button"
-            onClick={toggleTheme}
-            className="border-2 p-2 border-black rounded-md dark:border-gray-100 relative w-10 h-10"
-          >
-            {isDark ? (
-              <SunIcon className="absolute text-gray-100 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 " />
-            ) : (
-              <MoonIcon className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 " />
-            )}
-          </button>
+          <ThemeSwitch />
         </div>
+      </div>
+
+      <div className="bg-white dark:bg-slate-900 flex flex-col w-11/12 max-w-lg p-8 rounded-md fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col justify-center items-center gap-2">
+          <Logo />
+          <span className="bg-gradient-to-b from-pink-400 to-blue-700 text-transparent bg-clip-text font-bold">
+            Polon League
+          </span>
+        </div>
+        <h2 className="text-2xl text-center font-bold dark:text-gray-100 mt-8">
+          Welcome Summoner
+        </h2>
+        <h1 className="text-sm text-slate-500 font-medium text-center mt-1">
+          Enter your summoner name to start
+        </h1>
+
+        <form onSubmit={onSubmit} className="flex flex-col mt-8 gap-6">
+          <input
+            type="text"
+            name="summoner-name"
+            placeholder="Summoner name"
+            className="border border-slate-400 rounded px-3 py-4"
+            required
+          />
+          <button
+            type="submit"
+            className="text-gray-100 py-4 font-medium rounded-lg bg-gradient-to-b from-pink-400 to-blue-700 hover:brightness-[0.9] transition-all"
+          >
+            Continue
+          </button>
+        </form>
       </div>
 
       <div className="fixed inset-0 -z-10 pointer-events-none">
